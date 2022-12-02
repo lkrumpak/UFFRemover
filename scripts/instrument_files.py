f419ff34f56e5d2b5722eb50af6ede5e91bbffa9
lkrumpak <l.krumpak@student.vu.nl>
2022-12-02 15:26:25 +0100

    command runs only on the refrenced js files

--- scripts/instrument_files.py
+++ scripts/instrument_files.py
@@ -1,15 +1,35 @@
 import os
 
 
-def instrument_file(uff_path, dir_path):
-    for root, dirs, files in os.walk(dir_path):
-        for file in files:
-            if file.endswith(".js"):
-                path = os.path.join(root, file)
-                command = "node " + uff_path + " instrument_file " + path
-                os.system(command)
-                print("running command: " + command)
+# def instrument_file(uff_path, dir_path):
+#     for root, dirs, files in os.walk(dir_path):
+#         for file in files:
+#             if file.endswith(".js"):
+#                 path = os.path.join(root, file)
+#                 command = "node " + uff_path + " instrument_file " + path
+#                 os.system(command)
+#                 print("running command: " + command)
+
+def parse_html(dir_path):
+    paths = []
+    with open(dir_path + '/index.html', 'rt') as f:
+        data = f.readlines()
+    for line in data:
+        if '<script src=' in line:
+            path = line.split('"')[1]
+            paths.append(path)
+    return paths
 
 
 if __name__ == '__main__':
-    instrument_file('../../UFFRemover', '../subjects')
+    uff_path = '../../UFFRemover'
+
+    for webapp in next(os.walk('../subjects'))[1]:
+        proj_path = '../subjects/' + webapp
+        js_paths = parse_html(proj_path)
+
+        for js_path in js_paths:
+            command = "node " + uff_path + " instrument_file " + proj_path + "/" +js_path
+            #os.system(command)
+            print("running command: " + command)
+
